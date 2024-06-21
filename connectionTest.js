d1f5310c0f042b6aac3870a5be6793f78a9802ae
wanttocodegreatness <116949238+wanttocodegreatness@users.noreply.github.com>
2024-06-21 23:21:54 +0530

    Add files via upload

---
+++ connectionTest.js
@@ -0,0 +1,11 @@
+
+const mongoose= require('mongoose')
+const URL="mongodb+srv://admin:admin123@cluster0.g8gh9tm.mongodb.net/gfgdb?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(URL)
+
+const user=mongoose.model('demousers',{name:String,email:String})
+const myUser= new user({name:'admin',email:'admin@mail.com'})
+myUser.save().then(()=> console.log('user added'))
+
+
---
+++ models/model.js
@@ -0,0 +1,16 @@
+
+
+const mongoose = require('mongoose')
+
+
+const empc = mongoose.model('empCRUD',
+
+    {
+        _id: String,
+        uname: String,
+        email: String,
+        city: String
+    }
+)
+module.exports = empc
+
